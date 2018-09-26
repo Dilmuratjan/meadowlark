bc14014709662667ca7f2f4d3d3dc75c58f94aea
Collin <dilmuratjan@bupt.edu.cn>
2018-09-26 14:52:06 +0800

    node modules

--- meadowlark.js
+++ meadowlark.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const app = express();
 const handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
-
+const fortune = require('./lib/fortune.js');
+const app = express();
 const port = 3000
-const fortunes = [
-	"Conquer your fears or they will conqur you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple."
-];
+
 
 app.engine('handlebars', handlebars.engine);
 
@@ -19,26 +13,17 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || port);
 
-app.get('/',(req,res)=>{
-	res.render('home');
+app.get('/', (req, res) => res.render('home'));
+
+app.get('/about', (req, res) => res.render('about', { fortune: fortune.getFortune() }));
+
+app.use((req, res) => {
+	res.status(404).render('404');
 });
 
-app.get('/about',(req,res)=>{
-	res.render('about', {fortune: fortunes[Math.floor(Math.random()*fortunes.length)]});
-});
-
-app.use((req, res)=>{
-	res.status(404);
-	res.render('404');
-});
-
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
 	console.error(err.stack);
-	res.type('text/plain');
-	res.status(500);
-	res.render('500');
+	res.type('text/plain').status(500).render('500');
 });
 
-app.listen(app.get('port'), ()=>{
-	console.log('Express started on http://localhost:' + app.get('port') + '; press Control-C to terminate.');	
-});
+app.listen(app.get('port'), () => console.log('Express started on http://localhost:' + app.get('port') + '; press Control-C to terminate.'));
