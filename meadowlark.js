8257281fbea54f95fcc54b6070f6caff93b3bfc8
Collin <dilmuratjan@bupt.edu.cn>
2018-09-26 11:36:11 +0800

    views and static files

--- meadowlark.js
+++ meadowlark.js
@@ -6,7 +6,7 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 
-
+app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 
